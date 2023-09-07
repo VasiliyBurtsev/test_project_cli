13eb8bc49994c4a87b03c1c32284fcf04d3bba5a
VasiliyBurtsev <143483566+VasiliyBurtsev@users.noreply.github.com>
2023-09-07 16:47:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-/*
- * main.cpp
- *
- *  Created on: 05.09.2023
- *      Author: tata
- */
-
 #include <iostream>
 #include <fstream>
 
@@ -13,11 +6,9 @@
 #include <cpprest/filestream.h>
 #include <cpprest/uri.h>
 #include <cpprest/json.h>
-//#include <library/library_cli.hpp>
 
 
 using namespace std;
-//using namespace library_cli;
 using namespace utility;
 using namespace web;
 using namespace web::http;
@@ -30,17 +21,23 @@ using namespace web::json;
 int main() {
 		
     
-    string branch1,branch2,arch;
+     string branch1,branch2,arch1,arch2;
     cout << "Enter the first branch ";
     cin >> branch1;
+    cout << "Enter the arch of first branch ";
+    cin >> arch1;
     cout << "Enter the second branch ";
     cin >> branch2;
-	
-    list_json js_str1 = readJSONAPI(REST_API,branch1,arch,method);
+    cout << "Enter the arch of second branch ";
+    cin >> arch2;
+	cout << "List of binary_packages of the first branch ";
+    list_json js_str1 = readJSONAPI(REST_API,branch1,arch1,method);
 	writeListJSON(js_str1);
-    list_json js_str2 = readJSONAPI(REST_API,branch2,arch,method);
+    cout << "List of binary_packages of the second branch ";
+    list_json js_str2 = readJSONAPI(REST_API,branch2,arch2,method);
 	writeListJSON(js_str2);
-	    //say_hello();
+    list<packbranch> reslist = ResListPack(js_str1,js_str2);
+    writeResJSON(reslist);
 	    return 0;
 }
 
